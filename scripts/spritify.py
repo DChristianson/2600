d225d931e5c755b7e626c856354353f2759aa46a
dchristianson <christianson.davidb@gmail.com>
2021-07-03 22:40:47 -0500

    attempt 2-way offset solver'

--- scripts/spritify.py
+++ scripts/spritify.py
@@ -9,9 +9,17 @@ from os import path
 import glob
 from PIL import Image
 import itertools
+from collections import namedtuple
+import queue
+from dataclasses import dataclass, field
 
 aseprite_path = '/Users/dchristianson/Library/Application Support/Steam/steamapps/common/Aseprite/Aseprite.app/Contents/MacOS'
 explicit_zero = False
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 def chunker(iterable, n):
     args = [iter(iterable)] * n
@@ -49,10 +57,13 @@ def complement(n, b):
 def hmove(n):
     return complement(-n, b=16) * 16
 
+CompressedBits = namedtuple('CompressedBits', ['scale', 'start_index', 'end_index', 'bits'])
+
 # compress an array of bits to a single byte at single, double or quad resolution
+# return tuple of 
 def compress8(bits):
-    start_index = len(bits) - 1
-    end_index = 0
+    start_index = len(bits)
+    end_index = -1
     for i, b in enumerate(bits):
         if 0 == b:
             continue
@@ -60,64 +71,67 @@ def compress8(bits):
             start_index = i
         if i > end_index:
             end_index = i
-    bit_length = end_index - start_index    
+    bits = bits[start_index:end_index + 1]
+    bit_length = len(bits)
     if (bit_length <= 8):
-        #start_index = max(0, end_index - 7)
-        end_bits = min(len(bits), start_index + 8)
-        bits = bits[start_index:end_bits]
-        pad = 8 - len(bits)
-        bits = bits + ((0,) * pad)
-        return (0, start_index, bits2int(bits))
+        return CompressedBits(1, start_index, end_index, bits)
     if (bit_length <= 16):
-        #start_index = max(0, end_index - 15)
-        end_bits = min(len(bits), start_index + 16)
-        bits = bits[start_index:end_bits]
-        pad = 16 - len(bits)
-        bits = bits + ((0,) * pad)
-        return (5, start_index, bits2int(reduce_bits(bits, 2)))
-    #start_index = max(0, end_index - 31)
-    end_bits = min(len(bits), start_index + 32)
-    bits = bits[start_index:end_bits]
-    pad = 32 - len(bits)
-    bits = bits + ((0,) * pad)
-    return (7, start_index, bits2int(reduce_bits(bits, 4)))
+        pad = bit_length % 2
+        bits = bits + ([0] * pad)
+        end_index += pad
+        return CompressedBits(2, start_index, end_index, reduce_bits(bits, 2))
+    pad = 4 - bit_length % 4
+    bits = bits + ([0] * pad)
+    end_index += pad
+    return CompressedBits(4, start_index, end_index, reduce_bits(bits, 4))
 
-def shift_word(word, offset):
-    if offset < 0:        
-        mask = (1 << (-offset)) - 1
-    else:
-        nbits = len(word)
-        mask = ((1 << nbits) - 1) - ((1 << (nbits - offset)) - 1)
-    if word & mask:
-        raise Exception()
-    if offset < 0:
-        return word >> (-offset)
-    else:
-        return word << offset
+def nusize(i):
+    if i == 1:
+        return 0
+    return i + 3
 
-def solve_for_hmove(tuples):
-    # find offsets
-    vars = [[], [], [], []]
-    for i in range(0, len(tuples) - 1):
-        if tuples[i + 1][2] == 0:
-            delta_offset = 0
-        else:
-            delta_offset = tuples[i + 1][1] - tuples[i][1]
-        vars[0].append(tuples[i][0])
-        vars[1].append(delta_offset)
-        vars[2].append(tuples[i][2])
-    vars[0].append(tuples[-1][0])
-    vars[1].append(0)
-    vars[2].append(tuples[-1][2])
+def paddings(a):
+    nbits = len(a.bits)
+    pad = 8 - nbits
+    for lpad in range(0, pad + 1):
+        rpad = pad - lpad
+        bits = [0] * lpad + a.bits + [0] * rpad
+        start_index = a.start_index - lpad * a.scale
+        end_index = a.start_index + 8 * a.scale
+        yield start_index, end_index, bits
 
-    print(vars[1])
-    # convert to hmove
-    vars[1] = list([hmove(var) for var in vars[1]])
-    vars[3] = list([a + b for a, b in zip(vars[0], vars[1])])
-    return vars
+def is_legal_hmove(i):
+    return i < 8 and i > -9
 
+@dataclass(order=True)
+class SolutionItem:
+    priority: int
+    steps: object = field(compare=False)
+    frontier: object = field(compare=False)
 
+def find_offset_solution(compressedbits):
+    solutions = queue.PriorityQueue()
+    base_priority = 10 * (len(compressedbits) - 1)
+    for a in paddings(compressedbits[0]):
+        solutions.put(SolutionItem(base_priority, [(None, 0, 0, a)], compressedbits[1:]))
 
+    while not solutions.empty():
+        item = solutions.get()
+        _, _, _, a = item.steps[-1]
+        b = item.frontier[0]
+        for candidate in paddings(b):
+            lmove = candidate[0] - a[0]
+            rmove = candidate[1] - a[1]
+            if not is_legal_hmove(lmove):# or not is_legal_hmove(rmove):
+                continue
+            next_step = (a, lmove, rmove, candidate)
+            if len(item.frontier) == 1:
+                return item.steps[1:] + [next_step]
+            else:
+                cost = item.priority + abs(lmove) + abs(rmove) - 10
+                next_solution = item.steps + [next_step]
+                solutions.put(SolutionItem(cost, next_solution, item.frontier[1:]))
+            
 # variable resolution sprite
 def emit_sprite8(varname, image, fp, width=24, reverse=False):
     if not image.mode == 'RGBA':
@@ -126,12 +140,27 @@ def emit_sprite8(varname, image, fp, width=24, reverse=False):
     rows = chunker(map(bit, data), width)
     if reverse:
         rows = [tuple(reversed(row)) for row in rows]
-    tuples = list([compress8(row) for row in rows])
-    vars = solve_for_hmove(tuples)
+
+    compressedbits = list([compress8(list(row)) for row in rows])
+    solution = find_offset_solution(compressedbits)
+
+    left_delta = list([step[1] for step in solution])
+    right_delta = list([step[2] for step in solution])
+    padded_bits = list([step[3][2] for step in solution])
+
+    print(left_delta)
+    print(right_delta)
+    print(padded_bits)
+    
+    nusizes = list([ nusize(cb.scale) for cb in compressedbits])
+    ctrl = list([hmove(offset) + size for offset, size in zip(left_delta, nusizes)])
+    rtrl = list([hmove(-offset) + size for offset, size in zip(right_delta, nusizes)])
+    graphics = list([bits2int(bits) for bits in padded_bits])
+
     # write output
-    for col in vars:
+    for col in [ctrl, rtrl, graphics]:
         value = ','.join([int2asm(word) for word in reversed(col)])
-        fp.write(f'\t\t\t\tbyte\t{value}; {len(col)}\n')
+        fp.write(f'    byte {value}; {len(col)}\n')
 
 # full sized sprite
 def emit_sprite24(varname, image, fp):
@@ -167,7 +196,8 @@ if __name__ == "__main__":
             with Image.open(filename, 'r') as image:
                 print(';image')
                 emit_sprite8(varname, image, out)
-                print(';reverse')
-                emit_sprite8(varname, image, out, reverse=True)
+                # 
+                # print(';reverse')
+                # emit_sprite8(varname, image, out, reverse=True)
 
         
